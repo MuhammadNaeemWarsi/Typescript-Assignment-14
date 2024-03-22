941d3443da0e124ef77082487d015123f75996ef
[muhammad naeem] <[naeemwarsi20@gmail/com]>
2024-03-23 01:00:37 +0500

    Assignment 14

---
+++ Q14.js
@@ -0,0 +1,6 @@
+"use strict";
+//Guest List: If you could invite anyone, living or deceased, to dinner, who would you invite? Make a list that includes at least three people you’d like to
+// invite to dinner. Then use your list to print a message to each person, inviting them to dinner.
+Object.defineProperty(exports, "__esModule", { value: true });
+let guestArr = ["Meer", "Amjad", "Abdul Basit", "Umair"];
+guestArr.map((item) => (console.log(`Dear ${item}, you are invited to a Dinner`)));
